2c0f2b4ad2b9d004ee930468c92f1432118d3da0
konari allolkar <konariallolkar@gmail.com>
2020-03-11 00:31:40 +0530

    first commit

---
+++ src/git/actions/Product.java
@@ -0,0 +1,31 @@
+package git.actions;
+
+public class Product 
+{
+	private int id;  
+	private String name;  
+	private float price;  
+	public int getId() {  
+		return id;  
+	}  
+	public void setId(int id) {  
+		this.id = id;  
+	}  
+	public String getName() {  
+		return name;  
+	}  
+	public void setName(String name) {  
+		this.name = name;  
+	}  
+	public float getPrice() {  
+		return price;  
+	}  
+	public void setPrice(float price) {  
+		this.price = price;  
+	}  
+
+	public String execute(){ 
+		System.out.println("Action is involked.");
+		return "success";  
+	}  
+}
